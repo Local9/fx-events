44b2e9febef221c536bc3a0518dc60bbed081d62
Lucas <lucassivebro@outlook.com>
2021-07-02 17:53:52 +0200

    Fix KeyValuePair items being un-initialized

--- src/generator/Serialization/KeyValuePairSerialization.cs
+++ src/generator/Serialization/KeyValuePairSerialization.cs
@@ -21,8 +21,8 @@ namespace Moonlight.Generators.Serialization
             var named = (INamedTypeSymbol) type;
             var types = named.TypeArguments;
 
-            code.AppendLine($"{SerializationEngine.GetQualifiedName(types[0])} key;");
-            code.AppendLine($"{SerializationEngine.GetQualifiedName(types[1])} value;");
+            code.AppendLine($"{SerializationEngine.GetQualifiedName(types[0])} key = default;");
+            code.AppendLine($"{SerializationEngine.GetQualifiedName(types[1])} value = default;");
 
             engine.AppendReadLogic(property, types[0], code, "key", location);
             engine.AppendReadLogic(property, types[1], code, "value", location);
