e4f529387713277996b5ea1178b2c9e2e7fda7f8
Lucas <lucassivebro@outlook.com>
2021-07-24 01:35:51 +0200

    Fix file namings

--- src/generator/SerializationGenerator.cs
+++ src/generator/SerializationGenerator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
@@ -8,6 +10,8 @@ namespace Lusive.Events.Generator
     [Generator]
     public class SerializationGenerator : ISourceGenerator
     {
+        private readonly List<string> _sources = new();
+
         public void Initialize(GeneratorInitializationContext context)
         {
             context.RegisterForSyntaxNotifications(() => GenerationEngine.Instance);
@@ -18,16 +22,13 @@ namespace Lusive.Events.Generator
             var engine = (GenerationEngine) context.SyntaxContextReceiver;
 
             if (engine == null) return;
-            
+
             foreach (var item in engine.WorkItems)
             {
                 var code = engine.Compile(item);
-                var unique = $"{item.TypeSymbol.Name}.Serialization.cs";
-
-                if (item.TypeSymbol.ContainingType != null)
-                {
-                    unique = item.TypeSymbol.ContainingType.Name + "." + unique;
-                }
+                var identifier = item.TypeSymbol.Name;
+                var count = _sources.Count(self => self == identifier);
+                var unique = $"{identifier}{(count != 0 ? Convert.ToChar(65 + count) : string.Empty)}.Serialization.cs";
 
                 foreach (var problem in engine.Problems)
                 {
@@ -47,19 +48,20 @@ namespace Lusive.Events.Generator
                 }
 
                 engine.Problems.Clear();
-                
+
                 try
                 {
-                    context.AddSource(unique,
-                        SourceText.From(code.ToString(), Encoding.UTF8));
+                    context.AddSource(unique, SourceText.From(code.ToString(), Encoding.UTF8));
+
+                    _sources.Add(identifier);
                 }
                 catch (ArgumentException)
                 {
-                        throw new Exception(
-                            $"Duplicate entry: {item.TypeSymbol.ContainingNamespace}.{item.TypeSymbol.MetadataName}");
+                    throw new Exception(
+                        $"Duplicate entry '{item.TypeSymbol.ContainingNamespace}.{item.TypeSymbol.MetadataName}' ({unique})");
                 }
             }
-            
+
             // context.AddSource("Logs.cs",
             //     SourceText.From(
             //         $@"/*{Environment.NewLine + string.Join(Environment.NewLine, engine.Logs) + Environment.NewLine}*/",
